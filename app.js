71a1e9796983edfdfc4c0bc2d487e2a3eb23983b
smartrove <olaokunolalekan@gmail.com>
2023-08-17 13:54:05 +0100

    latest commit

---
+++ app.js
@@ -0,0 +1,79 @@
+// console.log("Hello world");
+
+// console.log("Aishah is beautiful");
+
+//variables and values
+//Variables are Containers for Storing Data or Values.
+//declaration of variables, we have var, let, const...
+
+// var aishah = 2; // data is the same as value;
+
+// aishah = 3; 
+
+// aishah = "Aishah"
+// console.log(aishah)
+
+// let a = 4 + 5;
+// let x = 8;
+// console.log(a  * x)
+
+
+// const y =  5 ** 2; // 5 * 5
+// console.log(y);
+
+
+//rules for variables declaration
+// 1. a variable must not begin with capital letter
+// 2. you can begin a variable with underscore (_).// e.g let _x = 123
+// 3. you can begin a variable with dollar sign ($). // e.g var $w = 123
+//4. joined words can be in camel case, pascal case and snake case
+//camel case
+// const ageAishah = 89;
+
+//pascal case
+// const AgeAishah = 89; // dont use this in JS
+
+// snake case
+// const age_aishah = 89;
+
+//data types
+// two types of data
+// primitive data types and non-primitive data types
+//primitive data types
+
+
+// -string
+// const name = "Shade"
+// console.log(typeof name)
+
+// let stringNumber = "2"
+// console.log(typeof stringNumber)
+
+// -number 
+
+// let x = 1;
+// console.log(typeof x)
+ 
+// let y = 9000;
+// console.log(typeof y)
+
+// - boolean
+// const isLoading = false;
+// console.log(typeof isLoading)
+
+// - undefined
+// let x;
+// console.log(typeof x)
+
+//null
+// const number = null;
+// console.log(number)
+
+// bigInt
+// let x = BigInt("123456789012345678901234567890");
+// console.log(typeof x)
+
+//empty 
+
+// const y = ""
+
